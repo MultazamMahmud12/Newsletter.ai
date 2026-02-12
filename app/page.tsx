6d7b517029ad6591003cdb7856a2948890523b4f
multazamsudad1234 <MultazamMahmud12>
2026-02-12 12:44:57 +0600

    frontend landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import Image from 'next/image';
-
+import { SignedIn,UserButton } from '@clerk/nextjs';
+import { Hero } from '@/components/landing/hero';
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className="min-h-screen bg-white dark:bg-black">
+      <Hero/>
+      {/*<Features />
+      <HowItWorks /> */}
+
+      <SignedIn>
+        <div className="fixed top-4 right-4">
+          <UserButton />
+        </div>
+      </SignedIn>
+
+      {/* <Pricing />
+      <CTA /> */}
+    </main>
   );
 }
